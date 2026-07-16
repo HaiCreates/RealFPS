76e7efd9162ae51f5aaf8e344600f1e477632e7f
Thiên Hải <133210843+thienhai123@users.noreply.github.com>
2026-07-16 11:43:51 +0700

    Add Xbox DVR support

--- RealFPS.bat
+++ RealFPS.bat
@@ -2,17 +2,41 @@
 title RealFPS - Windows Gaming Optimizer
 color 0A
 
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo.
+    echo Please run RealFPS as Administrator.
+    pause
+    exit /b
+)
+
 :MENU
 cls
 echo.
 echo =====================================
-echo   RealFPS - Windows Gaming Optimizer
+echo      RealFPS v0.1-alpha
+echo   Windows Gaming Optimizer
 echo =====================================
+echo.
+echo    Only Measurable Optimizations
+echo.
+echo    Real Tweaks
+echo    Real Results
+echo    No Placebo
+echo.
+echo    Created by Nguyen Thanh Thien Hai
 echo.
 echo POWER
 echo [1] Ultimate Performance
 echo [2] High Performance
 echo [3] Balanced
+echo.
+echo GAMING
+echo [4] Enable Game Mode
+echo [5] Disable Game Mode
+echo.
+echo [6] Disable Xbox DVR
+echo [7] Enable Xbox DVR
 echo.
 echo [0] Exit
 echo.
@@ -21,8 +45,15 @@ set /p choice=Choose an option:
 if "%choice%"=="1" goto ULTIMATE
 if "%choice%"=="2" goto HIGH
 if "%choice%"=="3" goto BALANCED
+if "%choice%"=="4" goto GAMEON
+if "%choice%"=="5" goto GAMEOFF
+if "%choice%"=="6" goto DVR_OFF
+if "%choice%"=="7" goto DVR_ON
 if "%choice%"=="0" exit
 
+echo.
+echo Invalid option.
+pause
 goto MENU
 
 :ULTIMATE
@@ -43,5 +74,35 @@ goto MENU
 powercfg /setactive 381b4222-f694-41f0-9685-ff5bb260df2e
 echo.
 echo Balanced Enabled
+pause
+goto MENU
+
+:GAMEON
+reg add "HKCU\Software\Microsoft\GameBar" /v AutoGameModeEnabled /t REG_DWORD /d 1 /f >nul
+echo.
+echo Game Mode Enabled
+pause
+goto MENU
+
+:GAMEOFF
+reg add "HKCU\Software\Microsoft\GameBar" /v AutoGameModeEnabled /t REG_DWORD /d 0 /f >nul
+echo.
+echo Game Mode Disabled
+pause
+goto MENU
+
+:DVR_OFF
+reg add "HKCU\System\GameConfigStore" /v GameDVR_Enabled /t REG_DWORD /d 0 /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v AppCaptureEnabled /t REG_DWORD /d 0 /f >nul
+echo.
+echo Xbox DVR Disabled
+pause
+goto MENU
+
+:DVR_ON
+reg add "HKCU\System\GameConfigStore" /v GameDVR_Enabled /t REG_DWORD /d 1 /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v AppCaptureEnabled /t REG_DWORD /d 1 /f >nul
+echo.
+echo Xbox DVR Enabled
 pause
 goto MENU
